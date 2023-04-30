d9a5cd7446dec3c7b06f058dd263acc830979ace
iancallo1 <ian.galaroza.callo@gmail.com>
2023-04-30 20:32:04 +0800

    Working part

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+Class StudentController extends Controller {
+    private $request;
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+    public function getUsers()
+    {
+        $users = User::all();
+        return response()->json(['data' => $users], 200);
+    }
+
+    public function add(Request $request){ //ADD USER
+        
+        $rules = [
+            'lastname' => 'required | max:50 | alpha_num',
+            'firstname'=> 'required | max:50 | alpha_num',
+            'middlename' => 'required | max:50 | alpha_num',
+            'age' => 'required | lt:50'
+
+        ];
+
+        $this->validate($request,$rules);
+
+        $user = User::create($request->all());
+        return response()->json($user, 200);
+    }
+    
+    public function updateUser(Request $request, $id) { //UPDATE USER
+        $rules = [
+          'lastname' => 'required | max:50 | alpha_num',
+          'firstname'=> 'required | max:50 | alpha_num',
+          'middlename' => 'required | max:50 | alpha_num',
+          'age' => 'required | lt:50'
+        ];
+    
+        $this->validate($request, $rules);
+    
+        $user = User::findOrFail($id);
+    
+        $user->fill($request->all());
+    
+        if ($user->isClean()) {
+            return response()->json("At least one value must
+            change", 403);
+        } else {
+            $user->save();
+            return response()->json($user, 200);
+        }
+    }
+
+
+    public function deleteUser($id) {
+        
+        $users = User::where('id', $id)->delete();
+        
+        if ($users){
+            return response()->json($users);
+        }
+        {
+            return $this -> errorResponse('user does not exits', Reponse::HTTP_NOT_FOUND);
+        }
+    }
+
+    public function showUser($id) {
+        
+        $users = User::where('id', $id)->first();
+        
+        if ($users){
+            return response()->json($users);}
+        {
+            return $this -> errorResponse('user does not exits', Reponse::HTTP_NOT_FOUND);
+        }
+    }
+
+}
